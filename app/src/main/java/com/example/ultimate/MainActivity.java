ebf1758a458af4bda0affe95ec37448511904531
Amruta <tapadiyaamruta@gmail.com>
2019-11-25 17:45:22 -0700

    full working just result need to be displaye

--- app/src/main/java/com/example/ultimate/MainActivity.java
+++ app/src/main/java/com/example/ultimate/MainActivity.java
@@ -1,28 +1,40 @@
 package com.example.ultimate;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 import android.hardware.Sensor;
 import android.hardware.SensorManager;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
-
+import android.widget.TextView;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.io.FileOutputStream;
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
 
 
 public class MainActivity extends AppCompatActivity implements SensorEventListener{
   //  private static final String TAG;
   private static final String TAG = "MainActivity";
+
     private Sensor SA;
     private Sensor SG;
     private SensorManager SM;
@@ -32,6 +44,8 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
         SM =(SensorManager)getSystemService(SENSOR_SERVICE);
         SA = SM.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
         SG = SM.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
@@ -39,38 +53,80 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         gyr= new JSONObject();
 
 
-        // mySensor= SM.getDefaultSensor(Sensor.TYPE_ALL);
-
     }
     public void StartSensor(View view){
+
         Toast.makeText(this, "Started", Toast.LENGTH_SHORT).show();
-
-
-
-        // mySensor= SM.getDefaultSensor(Sensor.TYPE_ALL);
         SM.registerListener((SensorEventListener) this, SA, SensorManager.SENSOR_DELAY_GAME);
         SM.registerListener((SensorEventListener) this,SG,SensorManager.SENSOR_DELAY_GAME);
 
 
     }
-    public void StopSensor(View view){
+    public void StopSensor(View view) throws JSONException {
+
         Toast.makeText(this, "Stopped", Toast.LENGTH_SHORT).show();
         SM.unregisterListener(this, SA);
         SM.unregisterListener(this, SG);
+        JSONArray jsonArray = new JSONArray();
+        jsonArray.put(acc);
+        jsonArray.put(gyr);
+        acc=new JSONObject();
+        gyr=new JSONObject();
+        sendPost(jsonArray);
+           }
+    public void sendPost(final JSONArray jsonArray) {
+        Thread thread = new Thread(new Runnable() {
+            @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+            @Override
+            public void run() {
+                try {
+                    URL url = new URL("http://atapadiy.pythonanywhere.com/predict");
+                    HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                    conn.setRequestMethod("POST");
+                    conn.setRequestProperty("Content-Type", "application/json;charset=UTF-8");
+                    conn.setRequestProperty("Accept","application/json");
+                    conn.setDoOutput(true);
+                    conn.setDoInput(true);
 
-        String aString= acc.toString();
-        String gString= gyr.toString();
-        Log.i(TAG,aString);
 
+                    System.out.println(jsonArray);
+                    DataOutputStream os = null;
+                    os=new DataOutputStream(conn.getOutputStream());
+                    os.writeBytes(jsonArray.toString());
 
+                    os.flush();
+                    os.close();
+
+                    Log.i("STATUS", String.valueOf(conn.getResponseCode()));
+                    Log.i("MSG" , conn.getResponseMessage());
+                    try(BufferedReader br = new BufferedReader(
+                            new InputStreamReader(conn.getInputStream(), "utf-8"))) {
+                        StringBuilder response = new StringBuilder();
+                        String responseLine = null;
+                        while ((responseLine = br.readLine()) != null) {
+                            response.append(responseLine.trim());
+                        }
+                        System.out.println(response.toString());
+                        Log.i("STRING MSG" ,response.toString() );
+                        String res=response.toString();
+                        //Toast.makeText(getApplicationContext(),res,Toast.LENGTH_SHORT).show();
+                    }
+                    conn.disconnect();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        thread.start();
     }
+
     @Override
     public void onSensorChanged(SensorEvent event) {
         String sensorName = event.sensor.getName();
         Toast.makeText(this, sensorName, Toast.LENGTH_SHORT).show();
 
-      //  Log.i(TAG,sensorName + ": X: " + event.values[0] + "; Y: " + event.values[1] + "; Z: " + event.values[2] + ";");
-        try{
+       try{
             if (sensorName== "BMI160_ACCELEROMETER Accelerometer Non-wakeup")
             {
                 acc.accumulate("ax",event.values[0]);
